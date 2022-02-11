32d0900bb3a6c41b2236e8b5a3ab4471beb9ce7a
JuanMndzStgo <84043074+JuanMndzStgo@users.noreply.github.com>
2022-02-11 09:03:51 -0600

    Actividad terminada
    Terminada

---
+++ jme/src/GuessNumberGame/GuessNumberMidlet.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GuessNumberGame;
+
+import java.util.Random;
+import javax.microedition.lcdui.Alert;
+import javax.microedition.lcdui.AlertType;
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Display;
+import javax.microedition.lcdui.Displayable;
+import javax.microedition.lcdui.TextBox;
+import javax.microedition.lcdui.TextField;
+import javax.microedition.midlet.*;
+
+/**
+ * @author mjuan
+ */
+public class GuessNumberMidlet extends MIDlet 
+        implements CommandListener{
+
+    private TextBox textbox;
+    private Command cmdExit;
+    private Command cmdTry;
+    private Alert alert;
+    private Random rand=new Random();
+    private int secretNumber;
+    private int numTrial=0;
+    private String msgPrompt="Adivine el numero (entero)";
+    
+    
+    public void startApp() {
+    textbox=new TextBox("Adivine el numero", msgPrompt,60, TextField.PLAIN);
+    cmdExit=new Command("Exito", Command.EXIT, 2);
+    cmdTry=new Command("Intentar", Command.SCREEN, 1);
+    textbox.addCommand(cmdTry);
+    textbox.addCommand(cmdExit);
+    textbox.setCommandListener(this);
+    alert =new Alert("Resultado","",null,AlertType.CONFIRMATION);
+    secretNumber=rand.nextInt(100);
+    Display.getDisplay(this).setCurrent(textbox);
+    }
+    
+    public void pauseApp() {
+    }
+    
+    public void destroyApp(boolean unconditional) {
+    }
+
+    public void commandAction(Command c, Displayable d) {
+       if(c==cmdTry){
+           textbox.delete(0, msgPrompt.length());
+           alert.setString(checkNumber(textbox.getString()));
+           Display.getDisplay(this).setCurrent(alert);
+           textbox.setString(msgPrompt);
+       }
+       if(c==cmdExit){
+           destroyApp(true);
+           notifyDestroyed();
+       }
+    }
+    
+    private String checkNumber(String strNumberIn){
+        int numberIn=Integer.parseInt(strNumberIn);
+        numTrial++;
+        if(numberIn==secretNumber){
+            int numTrialSaved=numTrial;
+            numTrial=0;
+            secretNumber= rand.nextInt(100);
+            return "Felicidades\n"+ "Lo conseguuiste en "+numTrialSaved+" Intentos";
+        }
+        if (numberIn>secretNumber){
+            return "Intenta un numero menor";
+        }else{
+            return "Intenta un numero mayor";
+        }
+    }
+}
---
+++ jme/src/lifecycle/LifeCycleMidlet.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lifecycle;
+
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Display;
+import javax.microedition.lcdui.Displayable;
+import javax.microedition.lcdui.Form;
+import javax.microedition.lcdui.StringItem;
+import javax.microedition.midlet.*;
+
+/**
+ * @author mjuan
+ */
+public class LifeCycleMidlet extends MIDlet implements CommandListener {
+    private Form formulario;
+    private Display pantalla;
+    private Command cmdExit;
+    private StringItem message;
+    
+    public LifeCycleMidlet(){ 
+        System.out.println("Llamamos a la inicializacion del midlet");
+    formulario=new Form("Midlet lifecycle");
+    message=new StringItem(null, "Construyendo el Midlet");
+    formulario.append(message);
+    cmdExit=new Command("Salir", Command.EXIT, 1);
+    formulario.addCommand(cmdExit);
+    formulario.setCommandListener(this);
+    pantalla=Display.getDisplay(this);
+    pantalla.setCurrent(formulario);
+    }
+    public void startApp() {
+System.out.println("StartApp() is called");
+message.setText("startApp es llamado...");
+    }
+    
+    public void pauseApp() {
+      System.out.println("PauseApp() is called");
+message.setText("pauseApp es llamado...");  
+    }
+    
+    public void destroyApp(boolean unconditional) {
+        System.out.println("DestroyApp() is called");
+message.setText("DestroyApp es llamado...");
+    }
+
+    public void commandAction(Command c, Displayable d) {
+     if(c == cmdExit){
+         destroyApp(true);
+         notifyDestroyed();
+     }   
+    }
+
+}
